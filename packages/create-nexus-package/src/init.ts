618aa52f7405be0761afc970592ea2573f95410a
Marius Craciunoiu <marius@uplift.ltd>
2020-08-25 17:14:09 -0600

    Better init

--- packages/create-nexus-package/src/init.ts
+++ packages/create-nexus-package/src/init.ts
@@ -2,16 +2,20 @@
 
 import path from "path";
 import fs from "fs";
+import mkdirp from "mkdirp";
 
-function initFiles() {
+const indexTemplate = fs.readFileSync(path.resolve(__dirname, "../src/fixtures/index.ts"));
+const todoTemplate = fs.readFileSync(path.resolve(__dirname, "../src/fixtures/todo.ts"));
+
+export function initFiles() {
   const pkgPath = path.resolve("package.json");
-
-  const name = path.parse(process.cwd()).name;
+  const pkgDir = path.dirname(pkgPath);
+  const pkgName = path.parse(pkgDir).name;
 
   const pkg = {
-    name: `@uplift-ltd/${name}`,
+    name: `@uplift-ltd/${pkgName}`,
     version: "0.1.0",
-    description: name,
+    description: pkgName,
     license: "UNLICENSED",
     author: "Uplift Agency Ltd. <npm@uplift.ltd>",
     main: "./dest/index.js",
@@ -22,12 +26,12 @@ function initFiles() {
     repository: {
       type: "git",
       url: "git+https://github.com/uplift-ltd/nexus.git",
-      directory: `packages/${name}`,
+      directory: `packages/${pkgName}`,
     },
     bugs: {
-      url: `https://github.com/uplift-ltd/nexus/issues?q=is:open+is:issue+label:${name}`,
+      url: `https://github.com/uplift-ltd/nexus/issues?q=is:open+is:issue+label:${pkgName}`,
     },
-    homepage: `https://nexus.uplift.sh/docs/packages/${name}`,
+    homepage: `https://nexus.uplift.sh/docs/packages/${pkgName}`,
     scripts: {
       build: "tsc",
       test: "jest",
@@ -38,4 +42,13 @@ function initFiles() {
   };
 
   fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2));
+
+  const srcDir = path.join(pkgDir, "src");
+
+  mkdirp.sync("srcDir");
+
+  fs.writeFileSync(path.join(srcDir, "index.ts"), indexTemplate);
+  fs.writeFileSync(path.join(srcDir, "todo.ts"), todoTemplate);
 }
+
+initFiles();
