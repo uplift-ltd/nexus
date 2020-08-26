46ddef6f3fc149bf2cd1aefbf8435676bffd4dfc
Marius Craciunoiu <marius@uplift.ltd>
2020-08-26 17:42:15 -0600

    Fix log

--- packages/create-nexus-package/src/init.ts
+++ packages/create-nexus-package/src/init.ts
@@ -16,18 +16,19 @@ export async function initFiles() {
   const relativePaths = fixturePaths.map((fixturePath) => fixturePath.replace(/.+fixtures\//, ""));
   const pkgPaths = relativePaths.map((relativePath) => path.resolve(pkgDir, relativePath));
 
-  console.log(fixturePaths);
-
   await Promise.all(
-    zip(fixturePaths, pkgPaths).map(async ([fixturePath, destPath]) => {
-      if (!fixturePath || !destPath) {
-        throw new Error("Lodash types are wrong");
-      }
+    zip(fixturePaths, pkgPaths, relativePaths).map(
+      async ([fixturePath, destPath, relativePath]) => {
+        if (!fixturePath || !destPath || !relativePath) {
+          throw new Error("Lodash types are wrong");
+        }
 
-      const fixture = await fs.promises.readFile(fixturePath as string, "utf-8");
-      await mkdirp(path.dirname(destPath));
-      await fs.promises.writeFile(destPath, ejs.render(fixture, { PACKAGE_NAME: pkgName }));
-    })
+        const fixture = await fs.promises.readFile(fixturePath as string, "utf-8");
+        await mkdirp(path.dirname(destPath));
+        await fs.promises.writeFile(destPath, ejs.render(fixture, { PACKAGE_NAME: pkgName }));
+        console.info(`Wrote ${relativePath}`);
+      }
+    )
   );
 }
 
