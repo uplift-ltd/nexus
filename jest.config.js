b6cd68cbc08d35b02d2b765d9f33e914b0630b0e
Marius Craciunoiu <marius@uplift.ltd>
2020-08-31 21:28:05 -0600

    Add root node_modules to jest config

--- jest.config.js
+++ jest.config.js
@@ -86,6 +86,8 @@ module.exports = {
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
 
+  modulePaths: [path.resolve(__dirname, "node_modules")],
+
   // Activates notifications for test results
   // notify: false,
 
