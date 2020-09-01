056e57d97c254b470808822d95016710b3f2b50d
Marius Craciunoiu <marius@uplift.ltd>
2020-09-01 12:29:24 -0600

    Export formik builtins

--- packages/formik/src/index.ts
+++ packages/formik/src/index.ts
@@ -1,3 +1,5 @@
 export type { FormikStatus } from "./status";
 export { EnhancedFormik } from "./EnhancedFormik";
 export { useEnhancedFormik } from "./useEnhancedFormik";
+
+export * from "formik";
