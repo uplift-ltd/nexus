bd1074304fb7bb34660c92d4f05cd9dd480b4d24
Camron Flanders <camron.flanders@gmail.com>
2020-09-10 10:05:50 -0500

    Update index.ts

--- packages/formik/src/index.ts
+++ packages/formik/src/index.ts
@@ -1,6 +1,6 @@
 export type { FormikStatus } from "./status";
 export { EnhancedFormik } from "./EnhancedFormik";
 export { useEnhancedFormik } from "./useEnhancedFormik";
-export { FormikConfigWithOverrides } from "./types";
+export type { FormikConfigWithOverrides } from "./types";
 
 export * from "formik";
