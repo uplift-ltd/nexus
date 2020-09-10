0123f5db20e2e3c2e17bb3916a2960b443bbe5b9
Camron Flanders <camron.flanders@gmail.com>
2020-09-10 09:42:50 -0500

    export EnhancedFormik Config interface

--- packages/formik/src/index.ts
+++ packages/formik/src/index.ts
@@ -1,5 +1,6 @@
 export type { FormikStatus } from "./status";
 export { EnhancedFormik } from "./EnhancedFormik";
 export { useEnhancedFormik } from "./useEnhancedFormik";
+export { FormikConfigWithOverrides } from "./types";
 
 export * from "formik";
