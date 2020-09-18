786b69494635727a83f2e9a7e99f23dcf43f493f
Marius Craciunoiu <marius@uplift.ltd>
2020-09-17 23:20:40 -0600

    Add useAfterDelay

---
+++ packages/use-after-delay/src/useAfterDelay.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+export const useAfterDelay = (delayMs?: number) => {
+  const [pastDelay, setPastDelay] = useState(!delayMs);
+
+  // set a timeout on mount to delay showing the loader. By setting delayMs to
+  // a number greater than 0 we can reduce the number of flashes and only show
+  // the loader when waiting a long time. Studies show that a loader that flashes
+  // for just a split second will make the app feel slower than not showing a loader
+  // during a very small wait
+  // eslint-disable-next-line consistent-return
+  useEffect(() => {
+    if (delayMs) {
+      const timeoutId = setTimeout(() => {
+        setPastDelay(true);
+      }, delayMs);
+      return () => {
+        clearTimeout(timeoutId);
+      };
+    }
+  }, [delayMs]);
+
+  return pastDelay;
+};
