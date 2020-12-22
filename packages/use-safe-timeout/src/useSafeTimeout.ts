d549cd6c48828c2013b2e17bca641df830718fac
Marius Craciunoiu <marius@uplift.ltd>
2020-12-21 17:50:49 -0700

    Add useSafeTimeout hook

---
+++ packages/use-safe-timeout/src/useSafeTimeout.ts
@@ -0,0 +1,31 @@
+import { useEffect, useRef } from "react";
+
+export function useSafeTimeout() {
+  const timeoutIds = useRef<number[]>([]);
+
+  useEffect(() => {
+    return () => {
+      timeoutIds.current.forEach((timeoutId) => {
+        clearTimeout(timeoutId);
+      });
+      timeoutIds.current = [];
+    };
+  }, []);
+
+  const startTimeout = (callback: () => void, ms: number) => {
+    const timeoutId = setTimeout(callback, ms);
+
+    timeoutIds.current = [...timeoutIds.current, timeoutId];
+
+    const cancelTimeout = () => {
+      if (timeoutIds.current.includes(timeoutId)) {
+        clearTimeout(timeoutId);
+        timeoutIds.current = timeoutIds.current.filter((id) => id !== timeoutId);
+      }
+    };
+
+    return cancelTimeout;
+  };
+
+  return startTimeout;
+}
