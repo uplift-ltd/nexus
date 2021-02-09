cddab8d12d3c00b22e4c59086c28390425388cc8
Marius Craciunoiu <marius@uplift.ltd>
2021-02-08 20:11:51 -0700

    feat: add useKeyboardVisible hook (#89)

---
+++ packages/use-keyboard-visible/src/useKeyboardVisible.ts
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import { Keyboard } from "react-native";
+
+export enum KeyboardShowEvents {
+  keyboardWillShow = "keyboardWillShow",
+  keyboardDidShow = "keyboardDidShow",
+}
+
+export enum KeyboardHideEvents {
+  keyboardWillHide = "keyboardWillHide",
+  keyboardDidHide = "keyboardDidHide",
+}
+
+interface UseKeyboardVisibleOptions {
+  showEvent?: KeyboardShowEvents;
+  hideEvent?: KeyboardHideEvents;
+}
+
+export function useKeyboardVisible({
+  showEvent = KeyboardShowEvents.keyboardWillShow,
+  hideEvent = KeyboardHideEvents.keyboardDidHide,
+}: UseKeyboardVisibleOptions) {
+  const [keyboardVisible, setKeyboardVisible] = useState(false);
+
+  useEffect(() => {
+    const showListener = Keyboard.addListener(showEvent, () => {
+      setKeyboardVisible(true);
+    });
+    const hideListener = Keyboard.addListener(hideEvent, () => {
+      setKeyboardVisible(false);
+    });
+
+    return () => {
+      hideListener.remove();
+      showListener.remove();
+    };
+  }, [showEvent, hideEvent]);
+
+  return keyboardVisible;
+}
---
+++ website/docs/packages/use-keyboard-visible.md
@@ -0,0 +1,39 @@
+---
+title: use-keyboard-visible
+---
+
+## Installation
+
+    yarn add @uplift-ltd/use-keyboard-visible
+
+## API
+
+### useKeyboardVisible
+
+Hook for react-native keyboard visibility.
+
+```ts
+import { useKeyboardVisible } from "@uplift-ltd/use-keyboard-visible";
+
+function MyComponent() {
+  const keyboardVisible = useKeyboardVisible();
+}
+```
+
+Defaults to using `keyboardWillShow` and `keyboardWillHide` events. Can pass in `keyboardDidShow`
+and `keyboardDidHide` instead.
+
+```ts
+import {
+  useKeyboardVisible,
+  KeyboardShowEvents,
+  KeyboardHideEvents,
+} from "@uplift-ltd/use-keyboard-visible";
+
+function MyComponent() {
+  const keyboardVisible = useKeyboardVisible({
+    showEvent: KeyboardShowEvents.keyboardDidShow,
+    hideEvent: KeyboardHideEvents.keyboardDidHide,
+  });
+}
+```
