a42ad5e47e180c8348a3b966c674852c1b3b6692
Marius Craciunoiu <marius@uplift.ltd>
2021-02-09 13:00:59 -0700

    fix: use will hide and allow to customize for keyboard safe area view

---
+++ packages/react-native-screen-views/CHANGELOG.md
@@ -0,0 +1,11 @@
+# Change Log
+
+All notable changes to this project will be documented in this file. See
+[Conventional Commits](https://conventionalcommits.org) for commit guidelines.
+
+## 1.1.0 (2021-02-09)
+
+### Features
+
+- add screen views ([#88](https://github.com/uplift-ltd/nexus/issues/88))
+  ([c0ed06a](https://github.com/uplift-ltd/nexus/commit/c0ed06a67da3bd7237d9ec7efd8557560b4d3caa))
--- packages/react-native-screen-views/src/KeyboardScreenSafeAreaView.tsx
+++ packages/react-native-screen-views/src/KeyboardScreenSafeAreaView.tsx
@@ -1,17 +1,20 @@
-import { useKeyboardVisible } from "@uplift-ltd/use-keyboard-visible";
+import { useKeyboardVisible, UseKeyboardVisibleOptions } from "@uplift-ltd/use-keyboard-visible";
 import React from "react";
 import { ScreenSafeAreaView, ScreenSafeAreaViewProps } from "./ScreenSafeAreaView";
 import { KEYBOARD_SCREEN_EDGES } from "./constants";
 
-interface KeyboardScreenSafeAreaViewProps extends ScreenSafeAreaViewProps {
-  keyboardEdges?: ScreenSafeAreaViewProps["edges"];
-}
+type KeyboardScreenSafeAreaViewProps = ScreenSafeAreaViewProps &
+  UseKeyboardVisibleOptions & {
+    keyboardEdges?: ScreenSafeAreaViewProps["edges"];
+  };
 
 export const KeyboardScreenSafeAreaView: React.FC<KeyboardScreenSafeAreaViewProps> = ({
   edges,
   keyboardEdges = KEYBOARD_SCREEN_EDGES,
+  showEvent,
+  hideEvent,
   ...props
 }) => {
-  const keyboardVisible = useKeyboardVisible();
+  const keyboardVisible = useKeyboardVisible({ showEvent, hideEvent });
   return <ScreenSafeAreaView edges={keyboardVisible ? keyboardEdges : edges} {...props} />;
 };
