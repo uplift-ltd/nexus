212bc93c883856da3193b0c9acfb7a253087e730
Marius Craciunoiu <marius@uplift.ltd>
2021-05-08 12:21:09 -0600

    feat: add support for nextjs env vars

--- packages/sentry/src/browser.ts
+++ packages/sentry/src/browser.ts
@@ -1,8 +1,8 @@
 import * as Sentry from "@sentry/browser";
 
-if (process.env.REACT_APP_SENTRY_PUBLIC_DSN) {
+if (process.env.NEXT_PUBLIC_SENTRY_DSN || process.env.REACT_APP_SENTRY_PUBLIC_DSN) {
   Sentry.init({
-    dsn: process.env.REACT_APP_SENTRY_PUBLIC_DSN,
+    dsn: process.env.NEXT_PUBLIC_SENTRY_DSN || process.env.REACT_APP_SENTRY_PUBLIC_DSN,
   });
 }
 
--- packages/sentry/src/index.ts
+++ packages/sentry/src/index.ts
@@ -1,8 +1,15 @@
 import * as Sentry from "@sentry/node";
 
-if (process.env.NODE_SENTRY_PUBLIC_DSN || process.env.REACT_APP_SENTRY_PUBLIC_DSN) {
+if (
+  process.env.NODE_SENTRY_PUBLIC_DSN ||
+  process.env.NEXT_PUBLIC_SENTRY_DSN ||
+  process.env.REACT_APP_SENTRY_PUBLIC_DSN
+) {
   Sentry.init({
-    dsn: process.env.NODE_SENTRY_PUBLIC_DSN || process.env.REACT_APP_SENTRY_PUBLIC_DSN,
+    dsn:
+      process.env.NODE_SENTRY_PUBLIC_DSN ||
+      process.env.NEXT_PUBLIC_SENTRY_DSN ||
+      process.env.REACT_APP_SENTRY_PUBLIC_DSN,
   });
 }
 
