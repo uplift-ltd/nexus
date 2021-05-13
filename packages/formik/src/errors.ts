545ec41b03bb8a481aa8917e370f2afa9cb4a604
Iulian Crudu <cruduiulik@gmail.com>
2021-05-13 17:46:02 +0300

    pr comments

--- packages/formik/src/errors.ts
+++ packages/formik/src/errors.ts
@@ -11,26 +11,29 @@ export interface ApplyErrorsToFieldsOptions {
   mapFieldName?: (fieldName: string) => string;
 }
 
+const identity = (field: string) => field;
+
 /**
  * Applies an array of field level errors from graphene to our formik fields for
  * display to the user.
  */
 export const getApplyErrorsToFields = (setErrors: FormikHelpers<unknown>["setErrors"]) => (
-  errors: GrapheneFieldError[],
-  options?: ApplyErrorsToFieldsOptions
+  errors?: GrapheneFieldError[],
+  { mapFieldName = identity }: ApplyErrorsToFieldsOptions = {}
 ) => {
   setErrors(
-    errors.reduce((acc, err) => {
-      const { mapFieldName } = options || {};
-      const errField = typeof mapFieldName === "function" ? mapFieldName(err.field) : err.field;
+    errors?.reduce((acc, err) => {
       return {
         ...acc,
-        [errField]: safeJoinWithComma(err.messages),
+        [mapFieldName(err.field)]: safeJoinWithComma(err.messages),
       };
-    }, {})
+    }, {}) || []
   );
 };
 
 export interface ErrorHelpers {
-  applyErrorsToFields: (errors: GrapheneFieldError[], options?: ApplyErrorsToFieldsOptions) => void;
+  applyErrorsToFields: (
+    errors?: GrapheneFieldError[],
+    options?: ApplyErrorsToFieldsOptions
+  ) => void;
 }
