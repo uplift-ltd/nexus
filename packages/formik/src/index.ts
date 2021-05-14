d74648579687bf2f183bca8995f52bcb7926e4ef
Iulian Crudu <cruduiulik@gmail.com>
2021-05-14 20:35:07 +0300

    Export everything

--- packages/formik/src/index.ts
+++ packages/formik/src/index.ts
@@ -1,11 +1,14 @@
-export type { FormikStatus, StatusHelpers } from "./status";
-export { EnhancedField } from "./EnhancedField";
-export type { EnhancedFieldAttributes, EnhancedFieldProps } from "./EnhancedField";
-export { EnhancedFormik } from "./EnhancedFormik";
+export * from "./status";
+export * from "./EnhancedField";
+export * from "./EnhancedFormik";
 export * from "./errors";
-export { useEnhancedField } from "./useEnhancedField";
-export { useEnhancedFormik } from "./useEnhancedFormik";
-export { useEnhancedFormikContext } from "./useEnhancedFormikContext";
-export type { FormikConfigWithOverrides } from "./types";
+export * from "./useEnhancedField";
+export * from "./useEnhancedFormik";
+export * from "./useEnhancedFormikContext";
+export type {
+  FormikConfigWithOverrides,
+  EnhancedFieldInputProps,
+  EnhancedFormikExtraProps,
+} from "./types";
 
 export * from "formik";
