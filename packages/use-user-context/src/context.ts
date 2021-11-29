30b3cbf51d591af42e334a70c9defd79fa6f844d
Marius Craciunoiu <marius@uplift.ltd>
2021-11-29 14:21:15 -0800

    feat: add user context refetch (#205)

--- packages/use-user-context/src/context.ts
+++ packages/use-user-context/src/context.ts
@@ -1,4 +1,4 @@
-import { ApolloError } from "@uplift-ltd/apollo";
+import { ApolloError, ApolloQueryResult, NetworkStatus } from "@uplift-ltd/apollo";
 import { createContext } from "react";
 import { CurrentUserShape } from "./types";
 
@@ -7,10 +7,20 @@ export interface UserContextShape<CurrentUser extends CurrentUserShape = any> {
   error?: ApolloError;
   isAuthenticated: boolean;
   currentUser: CurrentUser | null;
+  refetch(): Promise<ApolloQueryResult<CurrentUser>>;
+  refetching: boolean;
 }
 
 export const UserContext = createContext<UserContextShape>({
   loading: true,
   isAuthenticated: false,
   currentUser: null,
+  refetch: async () => {
+    return {
+      data: null,
+      loading: false,
+      networkStatus: NetworkStatus.ready,
+    };
+  },
+  refetching: false,
 });
