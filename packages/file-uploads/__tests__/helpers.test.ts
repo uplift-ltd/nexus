712ebd3f0c33ac8ce59ddc5d9b21becc607e4c73
Marius Craciunoiu <marius@uplift.ltd>
2021-11-30 10:48:36 -0800

    Cleanup

--- packages/file-uploads/__tests__/helpers.test.ts
+++ packages/file-uploads/__tests__/helpers.test.ts
@@ -3,7 +3,6 @@ import { getFileNameComponents } from "../src/helpers";
 describe("file-uploaders - Helpers", () => {
   it("should get correct file paths", () => {
     const result = ["image.jpg", "jpg", "image"];
-    // expect(EnhancedFormik).toBeDefined();
     expect(getFileNameComponents("file://blah/nested/image.jpg")).toStrictEqual(result);
     expect(getFileNameComponents("image.jpg")).toStrictEqual(result);
     expect(getFileNameComponents("https://uplift.ltd/nested/image.jpg")).toStrictEqual(result);
