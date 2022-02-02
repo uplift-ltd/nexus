c43b3b193d20b3913586294a7614e11112b2c320
Marius Craciunoiu <marius@uplift.ltd>
2022-02-02 08:49:25 -0700

    fix(ts-helpers): export all helpers (#223)

--- packages/ts-helpers/src/index.ts
+++ packages/ts-helpers/src/index.ts
@@ -1,4 +1,8 @@
+export type { ArrayElement } from "./arrayElement";
+export type { ArrayToUnion } from "./arrayToUnion";
+export type { DeepPartial } from "./deepPartial";
 export { notEmpty } from "./notEmpty";
 export type { RequireAtLeastOne, RequireOnlyOne } from "./require";
 export type { Unpromise } from "./unpromise";
 export { assertUnreachable, UnreachableCaseError } from "./unreachable";
+export type { ValuesOf } from "./valuesOf";
