26ed7dec69f700d751260e04a338c7ecf0d04e9d
Camron Flanders <camron.flanders@gmail.com>
2022-04-01 15:26:29 -0500

    default to string values, but allow overrides

--- packages/nextjs-use-react-navigation/src/useRouterQuery.ts
+++ packages/nextjs-use-react-navigation/src/useRouterQuery.ts
@@ -1,12 +1,20 @@
 import { makeQueryString, QueryStringParametersMap } from "@uplift-ltd/strings";
 import { useRouter } from "next/router";
 
+// prettier-ignore
+type RouterQueryResult<Shape extends string | Record<string, unknown>> =
+    [Shape] extends [string]
+        ? { [K in Shape]: string }
+        : { [K in keyof Shape]: Shape[K] extends Array<unknown> ? string[] : string };
+
 export function useRouterQuery<
-  QueryShape extends QueryStringParametersMap = QueryStringParametersMap
+  QueryShape extends string | QueryStringParametersMap = QueryStringParametersMap,
+  QueryResult = RouterQueryResult<QueryShape>,
+  UpdateQueryShape = Partial<[QueryShape] extends [string] ? QueryResult : QueryShape>
 >() {
   const router = useRouter();
 
-  const updateRouterQuery = (newQuery: Partial<QueryShape>) => {
+  const updateRouterQuery = (newQuery: UpdateQueryShape) => {
     const q = makeQueryString({ ...router.query, ...newQuery });
 
     if (q) {
@@ -17,7 +25,7 @@ export function useRouterQuery<
   };
 
   return {
-    routerQuery: router.query as QueryShape,
+    routerQuery: (router.query as unknown) as QueryResult,
     updateRouterQuery,
   };
 }
