d6b7a430e9ea5e8fe1fe37d453e6ce97bffa3858
Camron Flanders <camron.flanders@gmail.com>
2022-04-01 18:58:26 -0500

    support parsing URL for params to merge in

--- packages/nextjs-use-react-navigation/src/useRouterQuery.ts
+++ packages/nextjs-use-react-navigation/src/useRouterQuery.ts
@@ -1,16 +1,23 @@
-import { makeQueryString, QueryStringParametersMap } from "@uplift-ltd/strings";
+import { makeQueryString, QueryStringParametersMap, UrlTokens } from "@uplift-ltd/strings";
 import { useRouter } from "next/router";
 
 // prettier-ignore
-type RouterQueryResult<Shape extends string | Record<string, unknown>> =
-    [Shape] extends [string]
-        ? { [K in Shape]: string }
-        : { [K in keyof Shape]: Shape[K] extends Array<unknown> ? string[] : string };
+type RouterQueryResult<Tokens extends string, QueryStringParams extends never | string | Record<string, unknown> = never> =
+    // set URL params to be string
+    { [K in Tokens]: string } &
+    // Iterate through params union or shape and make values optional
+    // Allows explicit overrides of value types if needed. example, for arrays
+    (
+        [QueryStringParams] extends [string]
+            ? { [K in QueryStringParams]?: string }
+            : { [K in keyof QueryStringParams]?: QueryStringParams[K] extends Array<unknown> ? string[] : string }
+    )
 
 export function useRouterQuery<
-  QueryShape extends string | QueryStringParametersMap = QueryStringParametersMap,
-  QueryResult = RouterQueryResult<QueryShape>,
-  UpdateQueryShape = Partial<[QueryShape] extends [string] ? QueryResult : QueryShape>
+  URL extends string,
+  QueryStringShape extends string | QueryStringParametersMap = QueryStringParametersMap,
+  QueryResult = RouterQueryResult<UrlTokens<URL>, QueryStringShape>,
+  UpdateQueryShape = Partial<[QueryStringShape] extends [string] ? QueryResult : QueryStringShape>
 >() {
   const router = useRouter();
 
