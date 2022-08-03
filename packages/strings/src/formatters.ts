b4e56ca7d9e764ddcb76f0138d571510ba84bc82
Camron Flanders <camron.flanders@gmail.com>
2022-08-03 14:44:30 -0500

    new function, deprecate original

--- packages/strings/src/formatters.ts
+++ packages/strings/src/formatters.ts
@@ -1,15 +1,26 @@
 import { trim } from "./pointFree";
 
-// pluralize :: String -> String -> Number -> String
-export const pluralize = (singular: string, plural: string, excludeCount = false) => (
-  count: number
-) => {
+export type MakePluralizerParams = {
+  singular: string;
+  plural?: string;
+  includeCount?: boolean;
+};
+export const makePluralizer = ({
+  singular,
+  plural: providedPlural,
+  includeCount = true,
+}: MakePluralizerParams) => (count: number) => {
+  const plural = providedPlural ?? `${singular}s`;
   const word = count === 1 ? singular : plural;
 
-  if (excludeCount) return word;
-
-  return `${count} ${word}`;
+  return includeCount ? `${count} ${word}` : word;
 };
+
+/**
+ * @deprecated: use makePluralizer instead
+ */
+export const pluralize = (singular: string, plural: string) =>
+  makePluralizer({ singular, plural, includeCount: true });
 
 export const formatUsCurrency = (dollars: number, hideCents = false) => {
   const hasCents = dollars % 1 !== 0;
