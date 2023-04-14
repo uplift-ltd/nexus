3b79b0b49f8850ef722ba39aa55aa49779e22dee
Marius Craciunoiu <marius@uplift.ltd>
2023-04-13 19:03:03 -0600

    fix: remove atob and btoa

--- packages/graphene/__tests__/parseGlobalId.test.ts
+++ packages/graphene/__tests__/parseGlobalId.test.ts
@@ -1,10 +1,6 @@
 import { parseGlobalId } from "../src/parseGlobalId.js";
 
 describe("parseGlobalId", () => {
-  beforeAll(() => {
-    global.atob = atob;
-  });
-
   it("should parse global id", () => {
     expect(parseGlobalId("VXNlcjox")).toEqual({ name: "User", id: "1" });
   });
