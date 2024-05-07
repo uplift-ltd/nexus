806f331661fac469b9e67de684fc0d9223ac7f0f
Marius Craciunoiu <marius@uplift.ltd>
2024-05-07 15:22:51 -0600

    feat: add option to set terminating link (#402)
    * feat: add option to set terminating link
    
    * cleanup

--- packages/apollo/src/configure.ts
+++ packages/apollo/src/configure.ts
@@ -46,6 +46,7 @@ export interface ConfigureClientOptions extends Omit<ApolloClientOptions<unknown
   onNetworkError?: (err: ApolloError["networkError"], operation: Operation) => void;
   onNotAuthorized?: (err: ApolloError["networkError"], operation: Operation) => void;
   removeToken?: () => void;
+  terminatingLink?: ApolloLink;
 }
 
 const defaultFetch = typeof window !== "undefined" ? window.fetch : undefined;
@@ -70,6 +71,7 @@ export const configureClient = ({
   onNetworkError,
   onNotAuthorized,
   removeToken,
+  terminatingLink,
   uri,
   ...otherOptions
 }: ConfigureClientOptions) => {
@@ -170,7 +172,9 @@ export const configureClient = ({
 
   const links: ApolloLink[] = [errorLink, retryLink, authLink, ...extraLinks];
 
-  if (batch) {
+  if (terminatingLink) {
+    links.push(terminatingLink);
+  } else if (batch) {
     links.push(
       new BatchHttpLink({
         batchInterval,
