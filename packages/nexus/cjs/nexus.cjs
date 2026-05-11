a59a2973f273d8665a2e870f4f3d15471786bedf
Marius Craciunoiu <marius@uplift.ltd>
2026-05-11 17:35:34 -0600

    chore: target es2025 and drop ignoreDeprecations
    Migrate tsconfig to the TS 6 defaults documented in the 5.x→6.0 guide:
    target es2025 and moduleResolution bundler. Removes the temporary
    ignoreDeprecations escape hatch. nexus cjs/esm regenerate without the
    old ES5 downlevel helpers.

--- packages/nexus/cjs/nexus.cjs
+++ packages/nexus/cjs/nexus.cjs
@@ -1,157 +1,81 @@
 #!/usr/bin/env node
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g = Object.create((typeof Iterator === "function" ? Iterator : Object).prototype);
-    return g.next = verb(0), g["throw"] = verb(1), g["return"] = verb(2), typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (g && (g = 0, op[0] && (_ = 0)), _) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-var commander_1 = require("commander");
-var execa_1 = require("execa");
-var renameCjsExtensions_js_1 = require("./renameCjsExtensions.cjs");
-var replaceProgramVersion_js_1 = require("./replaceProgramVersion.cjs");
-var program = new commander_1.Command();
+const commander_1 = require("commander");
+const execa_1 = require("execa");
+const renameCjsExtensions_js_1 = require("./renameCjsExtensions.cjs");
+const replaceProgramVersion_js_1 = require("./replaceProgramVersion.cjs");
+const program = new commander_1.Command();
 program.version("4.1.1");
 program
     .command("build-library")
     .option("--tsconfig <path>", "Path to tsconfig.json", "tsconfig.json")
-    .action(function (script, _options) { return __awaiter(void 0, void 0, void 0, function () {
-    var cjs, esm, err_1;
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0:
-                _a.trys.push([0, 4, , 5]);
-                return [4 /*yield*/, (0, execa_1.execa)("tsc", [
-                        "-p",
-                        script.tsconfig,
-                        "--outDir",
-                        "./cjs",
-                        "--module",
-                        "commonjs",
-                    ])];
-            case 1:
-                cjs = _a.sent();
-                if (cjs.all) {
-                    console.info(cjs.all);
-                }
-                return [4 /*yield*/, (0, execa_1.execa)("tsc", [
-                        "-p",
-                        script.tsconfig,
-                        "--outDir",
-                        "./esm",
-                        "--module",
-                        "es2020",
-                    ])];
-            case 2:
-                esm = _a.sent();
-                if (esm.all) {
-                    console.info(esm.all);
-                }
-                return [4 /*yield*/, (0, renameCjsExtensions_js_1.renameCjsExtensions)("./cjs")];
-            case 3:
-                _a.sent();
-                if (!cjs.all && !esm.all) {
-                    console.info("Nexus build-library done!");
-                }
-                return [3 /*break*/, 5];
-            case 4:
-                err_1 = _a.sent();
-                console.error(err_1);
-                process.exitCode = 1;
-                return [3 /*break*/, 5];
-            case 5: return [2 /*return*/];
+    .action(async (script, _options) => {
+    try {
+        const cjs = await (0, execa_1.execa)("tsc", [
+            "-p",
+            script.tsconfig,
+            "--outDir",
+            "./cjs",
+            "--module",
+            "commonjs",
+        ]);
+        if (cjs.all) {
+            console.info(cjs.all);
         }
-    });
-}); });
-program.command("clean-library").action(function (_script, _options) { return __awaiter(void 0, void 0, void 0, function () {
-    var clean, err_2;
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0:
-                _a.trys.push([0, 2, , 3]);
-                return [4 /*yield*/, (0, execa_1.execa)("git", ["clean", "-dfx", "cjs", "esm"])];
-            case 1:
-                clean = _a.sent();
-                if (clean.all) {
-                    console.info(clean.all);
-                }
-                else {
-                    console.info("Nexus clean-library done!");
-                }
-                return [3 /*break*/, 3];
-            case 2:
-                err_2 = _a.sent();
-                console.error(err_2);
-                process.exitCode = 1;
-                return [3 /*break*/, 3];
-            case 3: return [2 /*return*/];
+        const esm = await (0, execa_1.execa)("tsc", [
+            "-p",
+            script.tsconfig,
+            "--outDir",
+            "./esm",
+            "--module",
+            "es2020",
+        ]);
+        if (esm.all) {
+            console.info(esm.all);
         }
-    });
-}); });
-program.command("replace-program-version").action(function (_script, _options) { return __awaiter(void 0, void 0, void 0, function () {
-    var result, err_3;
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0:
-                _a.trys.push([0, 2, , 3]);
-                return [4 /*yield*/, (0, replaceProgramVersion_js_1.replaceProgramVersion)()];
-            case 1:
-                result = _a.sent();
-                if (result.length) {
-                    console.info(result
-                        .filter(function (_a) {
-                        var hasChanged = _a.hasChanged;
-                        return hasChanged;
-                    })
-                        .map(function (_a) {
-                        var file = _a.file;
-                        return file;
-                    })
-                        .join("\n"));
-                }
-                else {
-                    console.info("Nothing to replace.");
-                }
-                return [3 /*break*/, 3];
-            case 2:
-                err_3 = _a.sent();
-                console.error(err_3);
-                process.exitCode = 1;
-                return [3 /*break*/, 3];
-            case 3: return [2 /*return*/];
+        await (0, renameCjsExtensions_js_1.renameCjsExtensions)("./cjs");
+        if (!cjs.all && !esm.all) {
+            console.info("Nexus build-library done!");
         }
-    });
-}); });
+    }
+    catch (err) {
+        console.error(err);
+        process.exitCode = 1;
+    }
+});
+program.command("clean-library").action(async (_script, _options) => {
+    try {
+        const clean = await (0, execa_1.execa)("git", ["clean", "-dfx", "cjs", "esm"]);
+        if (clean.all) {
+            console.info(clean.all);
+        }
+        else {
+            console.info("Nexus clean-library done!");
+        }
+    }
+    catch (err) {
+        console.error(err);
+        process.exitCode = 1;
+    }
+});
+program.command("replace-program-version").action(async (_script, _options) => {
+    try {
+        const result = await (0, replaceProgramVersion_js_1.replaceProgramVersion)();
+        if (result.length) {
+            console.info(result
+                .filter(({ hasChanged }) => hasChanged)
+                .map(({ file }) => file)
+                .join("\n"));
+        }
+        else {
+            console.info("Nothing to replace.");
+        }
+    }
+    catch (err) {
+        console.error(err);
+        process.exitCode = 1;
+    }
+});
 program.parse(process.argv);
 exports.default = program;
